dc8b789199d97eb010780d5850e92f9eb1d36919
Azizbek Abubakirov <azizbekabubakirov@qq.com>
2025-10-03 15:47:22 +0500

    Add add.py, a simple mad libs style program

---
+++ add.py
@@ -0,0 +1,4 @@
+number1 = input("Hi, please, enter the first number:\n")
+number2 = input("Now, enter the second number:\n")
+sum = int(number1)+int(number2)
+print(f"The sum of {number1} and {number2} is {sum}.")
